3ba78adf0805c32efa72ea4cbd2d25a9fb00836a
misko183 <rtmike183@gmail.com>
2020-12-11 21:18:58 +0100

    Add new class. Class name is Person and add new person named Ivan.

---
+++ HousingEstate/Person.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace HousingEstate
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public string Surname{get; set;}
+        public int Age { get; set; }
+
+
+        public Person(string name, string surname, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+        }
+
+        public Person()
+        {
+            Name = "-1";
+            Surname = "-1";
+            Age = -1;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}\n{Surname}\n{Age}";
+        }
+    }
+}
---
+++ HousingEstate/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HousingEstate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person ivan = new Person("Ivan", "Sládeček", 53);
+            Console.WriteLine($"{ivan.ToString()}"); //jednotkaaaa jeeee zamonouuuu...ouuu jeeee
+        }
+    }
+}
