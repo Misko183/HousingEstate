bc0f950569d1830517a8ef4d5554e7f24cf58a9d
misko183 <rtmike183@gmail.com>
2020-12-11 21:39:10 +0100

    Add new class named Flat consist of flatnum, extent rooms and the last one i add Name because  i can and it's better.

--- HousingEstate/Flast.cs
+++ HousingEstate/Flast.cs
@@ -1,10 +1,40 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
 
 namespace HousingEstate
 {
-    class Flast
+    class Flat
     {
+        public string Name { get; set; }
+        public int FlatNum { get; set; }
+        public int Extent { get; set; }
+        public int Rooms { get; set; }
+
+
+        public Flat(string name, int flatNum, int extent, int rooms)
+        {
+            Name = name;
+            FlatNum = flatNum;
+            Extent = extent;
+            Rooms = rooms;
+        }
+
+        public Flat()
+        {
+            Name = "-1";
+            FlatNum = -1;
+            Extent = -1;
+            Rooms = -1;
+        }
+
+
+        
+        public override string ToString()
+        {
+            Console.WriteLine("-----------");
+            return $"{Name}\n{FlatNum} {Extent} {Rooms}";
+        }
     }
 }
--- HousingEstate/Program.cs
+++ HousingEstate/Program.cs
@@ -8,6 +8,8 @@ namespace HousingEstate
         {
             Person ivan = new Person("Ivan", "Sládeček", 53);
             Console.WriteLine($"{ivan.ToString()}"); //jednotkaaaa jeeee zamonouuuu...ouuu jeeee
+            Flat firts = new Flat("First", 1, 25, 3);
+            Console.WriteLine($"{ firts.ToString()}");
         }
     }
 }
